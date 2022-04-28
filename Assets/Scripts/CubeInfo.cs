e4e69e48299d184761bdbec1fa98371482e4f253
sem <78421702+Sem-Glu@users.noreply.github.com>
2022-04-28 11:44:27 +0200

    CubeGrafityDirection

--- Assets/Scripts/CubeInfo.cs
+++ Assets/Scripts/CubeInfo.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class CubeInfo : MonoBehaviour
 {
+    private Rigidbody m_Rb;
+    private Vector3 m_Direction;
+
     [Header("Materials")]
     [SerializeField] private Material m_Red;
     [SerializeField] private Material m_Blue;
     [SerializeField] private Material m_Yellow;
 
+    [Header("States")]
+    [SerializeField] private bool m_Moveing;
     public enum m_State {Horizontal, Vertical, Forward}
     public m_State m_CurrentState;
+
+    private void Awake()
+    {
+        m_Rb = GetComponent<Rigidbody>();
+    }
 
     private void Update()
     {
@@ -18,16 +28,29 @@ public class CubeInfo : MonoBehaviour
         {
             this.GetComponent<MeshRenderer>().material = m_Blue;
             gameObject.tag = "Horizontal";
+            m_Direction = new Vector3(-1, 0, 0);
         }
         else if (m_CurrentState == m_State.Vertical)
         {
             this.GetComponent<MeshRenderer>().material = m_Red;
             gameObject.tag = "Vertical";
+            m_Direction = new Vector3(0, -1, 0);
         }
         else if (m_CurrentState == m_State.Forward)
         {
             this.GetComponent<MeshRenderer>().material = m_Yellow;
             gameObject.tag = "Forward";
+            m_Direction = new Vector3(0, 0, -1);
         }
+
+        if (m_Moveing == true)
+        {
+        Move();
+        }
+    }
+
+    private void Move()
+    {
+        m_Rb.AddForce(m_Direction, ForceMode.Force);
     }
 }
